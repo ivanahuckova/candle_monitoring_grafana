502991c2357df8c103775d391203f15ba223f42f
Ivana <ivana.huckova@gmail.com>
2021-05-09 17:48:13 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -21,9 +21,8 @@ app.get('/', (_, res) => {
 });
 
 app.get('/toggle', (req, res) => {
-  const canToggle = toggleTimestamp < Date.now();
-  const isAuthorized = req.headers['secret-token'] === process.env.SECRET_TOKEN;
-  if (isAuthorized && canToggle) {
+  const canToggle = toggleTimestamp < Date.now() && req.params['secret'] === process.env.SECRET_TOKEN;
+  if (canToggle) {
     toggleTimestamp = Date.now() + 15000;
     candleIsOpen = !candleIsOpen;
     res.send(`Toggled state to ${candleIsOpen ? 'opened' : 'closed'}`);
