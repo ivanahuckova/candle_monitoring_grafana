35a6c9fecd47b33636a020a793c241315178b9b7
Ivana <ivana.huckova@gmail.com>
2021-05-09 16:33:41 +0200

    Add options route

--- server/index.js
+++ server/index.js
@@ -8,16 +8,18 @@ const app = express();
 let candleIsOpen = true;
 let toggleTimestamp = 0;
 
+//Corse & pre-flight
 app.use(cors());
+app.options('*', cors());
 //Routes
+
 app.get('/', (_, res) => {
   res.send('Welcome to 🕯 monitoring!');
 });
 
 app.get('/toggle', (req, res) => {
   const canToggle = toggleTimestamp < Date.now();
-  const isAuthorized = req.headers.authorization === process.env.AUTH_HEADER;
-  if (isAuthorized && canToggle) {
+  if (canToggle) {
     toggleTimestamp = Date.now() + 15000;
     candleIsOpen = !candleIsOpen;
     res.send(`Toggled state to ${candleIsOpen ? 'opened' : 'closed'}`);
